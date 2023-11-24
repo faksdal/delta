00a2fc6d7ae0af218c06dc944222261a64db3905
Jon Leithe <jon@jonleithe.no>
2023-11-24 17:02:57 +0100

    Added Celcius to Fahrenheit conversion

--- main.cpp
+++ main.cpp
@@ -27,18 +27,16 @@ int main(int argc, char *argv[])
 	bool	verbose = false;
 	char	*shortOptions = (char*)"";
 	float	float_sec;
-	//float	input = 0L, output;
+	float	input = 0L, output;
 	
 
 	struct option longOptions[] = {
 		{"degtodec",	required_argument,	NULL,	1},
 		{"dectodeg",	required_argument,	NULL,	2},
 		{"verbose",		no_argument,		NULL,	3},
-		/*
 		{"ctof",		required_argument,	NULL,	4},
 		{"ftoc",		required_argument,	NULL,	5},
 		{"help",		required_argument,	NULL,	6},
-		*/
 		{0, 0, 0, 0}
 	};
 	
@@ -55,35 +53,39 @@ int main(int argc, char *argv[])
 	while((c = getopt_long(argc, argv, shortOptions, longOptions, &optionIndex)) != -1){
 		switch(c){
 			case 1:		{
-						convertDegrees d(optarg, degToDec, '-', verbose);
+							convertDegrees d(optarg, degToDec, '-', verbose);
 
-						if(verbose)
-							printf("Conversion: %d°%d'%f'' =  %12.9f°\n", d.getDeg(), d.getMin(), d.getSec(), d.getDecDegrees());
-						else
-							printf("%12.9f°\n", d.getDecDegrees());
-						break;
+							if(verbose)
+								printf("Conversion: %d°%d'%f'' =  %12.9f°\n", d.getDeg(), d.getMin(), d.getSec(), d.getDecDegrees());
+							else
+								printf("%12.9f°\n", d.getDecDegrees());
+							break;
 						}
 			case 2:		{
-						convertDegrees d(optarg, decToDeg, '-', verbose);
+							convertDegrees d(optarg, decToDeg, '-', verbose);
 
-						if(verbose)
-							printf("Conversion: %12.9f° = %d°%d'%12.9f''\n", d.getDecDegrees(), d.getDeg(), d.getMin(), d.getSec());
-						else
-							printf("%d°%d'%12.9f''\n", d.getDeg(), d.getMin(), d.getSec());
-						break;
+							if(verbose)
+								printf("Conversion: %12.9f° = %d°%d'%12.9f''\n", d.getDecDegrees(), d.getDeg(), d.getMin(), d.getSec());
+							else
+								printf("%d°%d'%12.9f''\n", d.getDeg(), d.getMin(), d.getSec());
+							break;
 						}
 			case 3:		{
 							verbose = true;
 							break;
 						}
-			case 4:		//input = atof(optarg);
-						//output = ((input * 9/5) + 32);
-						//printf("%f°C = %f°F\n\n", input, output);
-						break;
-			case 5:		//input = atof(optarg);
-						//output = ((input - 32) * 5/9);
-						//printf("%f°F = %f°C\n\n", input, output);
-						break;
+			case 4:		{
+							input = atof(optarg);
+							output = ((input * 9/5) + 32);
+							printf("%f°C = %f°F\n\n", input, output);
+							break;
+						}
+			case 5:		{
+							input = atof(optarg);
+							output = ((input - 32) * 5/9);
+							printf("%f°F = %f°C\n\n", input, output);
+							break;
+						}
 			case 6:		//invoceatob()
 						printUsage();
 						break;
@@ -104,6 +106,6 @@ void printUsage(void)
 {
 	printf("Usage: delta --degtodec <deg>-<min>-<sec>\n");
 	printf("       delta --dectodeg <decimaldegrees>\n");
-	//printf("       dtd --ctof <degrees Celcius>\n");
-	//printf("       dtd --ftoc <degrees Fahrenheit>\n");
+	printf("       delta --ctof <degrees Celcius>\n");
+	printf("       delta --ftoc <degrees Fahrenheit>\n");
 }
